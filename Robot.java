00b2224038073e5d68051725bdcfcc9b788df17d
OsazeeOsaro <67610416+OsazeeOsaro@users.noreply.github.com>
2022-09-29 16:20:34 -0400

    Add files via upload

---
+++ Robot.java
@@ -0,0 +1,74 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.TimedRobot;
+
+/**
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public class Robot extends TimedRobot {
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
+  @Override
+  public void robotInit() {}
+
+  @Override
+  public void robotPeriodic() {}
+
+  @Override
+  public void autonomousInit() {}
+
+  @Override
+  public void autonomousPeriodic() {}
+
+  @Override
+  public void teleopInit() {}
+
+  @Override
+  public void teleopPeriodic() {
+    Joystick joystick = new Joystick(0);
+    TalonSRX motor = new TalonSRX(15);
+
+
+    motor.set(ControlMode.PercentOutput, joystick.getY() / 2);
+    VictorSPX motor2 = new VictorSPX(1);
+    motor2.set(ControlMode.PercentOutput, joystick.getY() / 2);
+    if (joystick.getY() < 0) {
+      motor.set(ControlMode.PercentOutput, -joystick.getY() / 2);
+      motor2.set(ControlMode.PercentOutput, -joystick.getY() / 2);
+      
+    }
+ 
+  }
+
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  @Override
+  public void testInit() {}
+
+  @Override
+  public void testPeriodic() {}
+
+  @Override
+  public void simulationInit() {}
+
+  @Override
+  public void simulationPeriodic() {}
+}
